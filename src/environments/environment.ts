7fc7c11ea089262f54ee3a6d6e0ccbe4c5b8bd09
EduardoCGarcia <echavezg004@gmail.com>
2024-04-04 22:13:23 -0600

    Environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBufS_60orGPpO5_BD8E5zWScb0jtziTcw",
+  authDomain: "inclutec-46d4e.firebaseapp.com",
+  projectId: "inclutec-46d4e",
+  storageBucket: "inclutec-46d4e.appspot.com",
+  messagingSenderId: "547225778373",
+  appId: "1:547225778373:web:7ca1b711bec5d9eb5158ac"
+};
