8186a2f1eb67913ec2e3fb78c3ec39dfef2f7eb1
EduardoCGarcia <echavezg004@gmail.com>
2024-04-04 22:12:50 -0600

    Testeo de conexi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirestoreSharedService } from './shared/services/firestore-shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'incluTec';
+
+  constructor(private fsSvc: FirestoreSharedService){}
+  ngOnInit(): void {
+    this.fsSvc.getAllDocuments('notes').subscribe(data =>{
+      console.log(data)
+    }
+    )
+  }
+
+
 }
